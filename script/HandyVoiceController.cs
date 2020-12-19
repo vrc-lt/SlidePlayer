b662c459a22fc875bf58ca1bb93ddd83ece2df99
haru2036 <haru2036@gmail.com>
2020-12-19 18:29:24 +0900

    add: Pickupに連動して距離減衰を調整するスクリプトを追加

---
+++ script/HandyVoiceController.cs
@@ -0,0 +1,44 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.SDKBase;
+using VRC.Udon;
+
+    public class HandyVoiceController: UdonSharpBehaviour
+    {
+        [UdonSynced]
+        bool enabled;
+
+        bool localEnabled;
+
+        public override void OnPickup(){
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            enabled = true;
+            Debug.Log("Loudspeaker activated");
+        }
+
+        public override void OnDrop(){
+            enabled = false;
+            Debug.Log("Loudspeaker deactivated");
+        }
+        public override void OnDeserialization()
+        {
+            VRCPlayerApi player = Networking.GetOwner(gameObject);
+            Debug.Log("OnDeserialization");
+            if(!Networking.IsOwner(gameObject)){
+                if(localEnabled != enabled){
+                    if(enabled){
+                        Debug.Log("Loudspeaker activated");
+                        player.SetVoiceDistanceFar(400f);
+                        player.SetAvatarAudioGain(7.5f);
+                    }else{
+                        Debug.Log("Loudspeaker deactivated");
+                        player.SetVoiceDistanceFar(25f);
+                        player.SetAvatarAudioGain(10f);
+                    }
+                    localEnabled = enabled;
+                }
+            }
+        }
+
+    }
